8674741ce1a14e3f10c04cf66b84a82aa0a28edb
Adam Neumann <noizwaves@users.noreply.github.com>
2021-04-10 21:02:19 -0600

    Install dependencies in containers when they change

---
+++ bin/install_dependencies.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+GEMFILE_SHA=$(sha256sum /app/Gemfile | cut -d ' ' -f 1)
+GEMFILE_TOUCH_PATH=/tmp/${GEMFILE_SHA}
+if [ ! -f ${GEMFILE_TOUCH_PATH} ]; then
+  bundle install
+  touch ${GEMFILE_TOUCH_PATH}
+fi
+
+YARN_SHA=$(sha256sum /app/package.json | cut -d ' ' -f 1)
+YARN_TOUCH_PATH=/tmp/${YARN_SHA}
+if [ ! -f ${YARN_TOUCH_PATH} ]; then
+  bundle install
+  touch ${YARN_TOUCH_PATH}
+fi
