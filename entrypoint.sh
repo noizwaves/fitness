f0d039a2997112655c9a37799c05ddf76df965a1
Adam Neumann <noizwaves@users.noreply.github.com>
2021-04-11 21:00:29 -0600

    Tweak commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,5 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /app/tmp/pids/server.pid
 
-if [ "$RAILS_ENV" = "development" ]; then
-  bin/rails db:migrate
-fi
-
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
