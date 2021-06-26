69192ed19c816f951985d0396475ef1c17a0c6fb
Adam Neumann <noizwaves@users.noreply.github.com>
2021-06-26 16:15:56 -0600

    Swap setsid for job control and bg+fg

--- bin/devspace-restart-helper.sh
+++ bin/devspace-restart-helper.sh
@@ -3,6 +3,7 @@
 # A process wrapper script to simulate a container restart. This file was injected with devspace during the build process
 #
 set -e
+set -m
 pid=""
 trap quit TERM INT
 quit() {
@@ -11,11 +12,11 @@ quit() {
   fi
 }
 while true; do
-  setsid "$@" &
+  "$@" &
   pid=$!
   echo "$pid" > /.devspace/devspace-pid
   set +e
-  wait $pid
+  fg
   exit_code=$?
   if [ -f /.devspace/terminated ]; then
     printf "\n\n############### Pod terminating, exiting ###############\n\nn"
