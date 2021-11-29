0bd4967801716160ad289d8975ba5f46a0117b37
Adam Neumann <noizwaves@users.noreply.github.com>
2021-11-28 18:37:47 -0700

    Attempt to pin Ruby version and WIP for NodeJS

--- default.nix
+++ default.nix
@@ -1,14 +1,31 @@
 with import <nixpkgs> {};
+let
+  ruby_2-7-3 = pkgs.ruby.overrideAttrs (oldAttrs: rec {
+    version = "2.7.3";
+    src = fetchurl {
+      url = "https://cache.ruby-lang.org/pub/ruby/2.7/ruby-2.7.3.tar.gz";
+      sha256 = "8925a95e31d8f2c81749025a52a544ea1d05dad18794e6828709268b92e55338";
+    };
+  });
 
-stdenv.mkDerivation {
+  nodejs_14-15-5 = pkgs.ruby.overrideAttrs (oldAttrs: rec {
+    version = "14.15.5";
+    src = fetchurl {
+      url = "https://nodejs.org/dist/v14.15.5/node-v14.15.5.tar.xz";
+      sha256 = "e19b1d40e958fe30c224f5a67af4ee4081e7f9d6fb586fb4bbc8d94aab39655b";
+    };
+  });
+in stdenv.mkDerivation {
   name = "fitness";
 
   buildInputs = [
     stdenv
 
-    (pkgs.callPackage ./ruby_2-7-3.nix {})
+    # (pkgs.callPackage ./ruby_2-7-3.nix {})
+    ruby_2-7-3
 
     nodejs-14_x
+    # nodejs_14-15-5
     yarn
 
     postgresql
