a926af49b8a9efc2301fd7c0a798e8ce03b6361c
Adam Neumann <noizwaves@users.noreply.github.com>
2021-12-04 15:24:05 -0700

    More progress on pinned 2.7.3 with overridden attrs

--- default.nix
+++ default.nix
@@ -1,11 +1,39 @@
-with import <nixpkgs> {};
 let
-  ruby_2-7-3 = pkgs.ruby.overrideAttrs (oldAttrs: rec {
+  pinned_release = builtins.fetchTarball {
+    # https://github.com/NixOS/nixpkgs/tree/21.05
+    name = "nixpkgs-92bff24dc157218068c10300f4b8af7db7af0fd6";
+    url = "https://github.com/NixOS/nixpkgs/archive/92bff24dc157218068c10300f4b8af7db7af0fd6.tar.gz";
+    # Hash obtained using `nix-prefetch-url --unpack <url>`
+    sha256 = "1hk664l92nba1dampmrs69sdfdvbhf51jpdnfgwkrqmcr5rsbygj";
+  };
+in
+with (import pinned_release {});
+# with import <nixpkgs> {};
+let
+  # pull in RVM patchsets directly
+  # https://www.reddit.com/r/NixOS/comments/6xg5aa/comment/dmjlgqb/?utm_source=share&utm_medium=web2x&context=3
+  patchSet = import <nixpkgs/pkgs/development/interpreters/ruby/rvm-patchsets.nix> { inherit fetchFromGitHub; };
+  doNotRegenerateRevisionH = <nixpkgs/pkgs/development/interpreters/ruby/do-not-regenerate-revision.h.patch>;
+
+  # ruby_2-7-3 = pkgs.ruby_2_7;
+  ruby_2-7-3 = pkgs.ruby_2_7.overrideAttrs (oldAttrs: rec {
     version = "2.7.3";
-    src = fetchurl {
-      url = "https://cache.ruby-lang.org/pub/ruby/2.7/ruby-2.7.3.tar.gz";
-      sha256 = "8925a95e31d8f2c81749025a52a544ea1d05dad18794e6828709268b92e55338";
+    src = fetchFromGitHub {
+      owner = "ruby";
+      repo = "ruby";
+      rev = "2.7.3";
+      sha256 = "0vxg9w4dgpw2ig5snxmkahvzdp2yh71w8qm49g35d5hqdsql7yrx";
     };
+    patches = [
+      "${patchSet}/patches/ruby/2.7/head/railsexpress/01-fix-broken-tests-caused-by-ad.patch"
+      "${patchSet}/patches/ruby/2.7/head/railsexpress/02-improve-gc-stats.patch"
+      "${patchSet}/patches/ruby/2.7/head/railsexpress/03-more-detailed-stacktrace.patch"
+      doNotRegenerateRevisionH
+      (fetchpatch {
+        url = "https://github.com/ruby/ruby/commit/261d8dd20afd26feb05f00a560abd99227269c1c.patch";
+        sha256 = "0wrii25cxcz2v8bgkrf7ibcanjlxwclzhayin578bf0qydxdm9qy";
+      })
+    ];
   });
 
   nodejs_14-15-5 = pkgs.ruby.overrideAttrs (oldAttrs: rec {
